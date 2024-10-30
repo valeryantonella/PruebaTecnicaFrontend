9382f4c25726766440c91f4e0bbd761f09f3510b
Valery Vallejo <valery.vallejo@epn.edu.ec>
2024-10-30 00:55:38 -0500

    chore: add App.tsx to test chakraui

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Box } from '@chakra-ui/react';
+import { MainRouter } from './components/MainRouter';
+
+export const App = () => {
+  return (
+    <Box
+      css={{
+        minHeight: '100vh',
+        maxWidth: '8xl',
+        px: { sm: 'md', md: 'none' },
+        mx: 'auto',
+        fontFamily: 'sans-serif',
+      }}
+    >
+      Bienvenidos a BIT
+    </Box>
+  );
+};
